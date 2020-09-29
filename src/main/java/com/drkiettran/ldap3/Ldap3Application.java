57e279b39ab22673432ceabf87b9ac0c744cd216
Kiet T. Tran, Ph.D <ktran@harrisburgu.edu>
2020-09-29 07:26:16 -0400

    Initial checkin for spring boot with sprint ldap

---
+++ src/main/java/com/drkiettran/ldap3/Ldap3Application.java
@@ -0,0 +1,13 @@
+package com.drkiettran.ldap3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ldap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ldap3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/drkiettran/ldap3/Ldap3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drkiettran.ldap3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ldap3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
